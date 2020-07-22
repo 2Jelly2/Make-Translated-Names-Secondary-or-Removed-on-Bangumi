6f410ac3faabb9d7ed4282632e8255d36c285d8e
2Jelly2 <2Jelly2@users.noreply.github.com>
2020-07-22 10:45:49 +0800

    Update Remove-Translated-Names-on-Bangumi.user.js

--- Remove-Translated-Names-on-Bangumi.user.js
+++ Remove-Translated-Names-on-Bangumi.user.js
@@ -1,37 +1,91 @@
 // ==UserScript==
 // @name         移除Bangumi译名
 // @namespace    https://github.com/2Jelly2/Remove-Translated-Names-on-Bangumi
-// @version      0.01
+// @version      0.02
 // @description  Remove Translated Names on Bangumi
 // @author       時計坂しぐれ
 // @match        https://chii.in/*/list/*
 // @match        https://bgm.tv/*/list/*
 // @match        https://bangumi.tv/*/list/*
+// @match        https://chii.in/subject/*
+// @match        https://bgm.tv/subject/*
+// @match        https://bangumi.tv/subject/*
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    // Your code here...
-var subjects = document.getElementsByTagName("h3");
+    var url = window.location.href;
 
-for (var i = 0; i <= subjects.length; i++)
-{
-    var full = subjects[i].innerHTML;
+    // Exchange original names and translated ones,
+    // with *false* to remove translated names without exchange.
+    var listPageModify = true;
 
-    if (full.match(/<small(([\s\S])*?)small>/g) != null)
+    // Translated names on Subject Pages are remained by default,
+    // with *true* to enable modification.
+    var subjectPageModify = false;
+
+    if (url.match(/(chii.in|bgm.tv|bangumi.tv)\/subject/) == null)
+    // Modify on List Pages
     {
-        var translatedTag = full.replace(/<small(([\s\S])*?)small>/g, '');
-        var translatedRaw = translatedTag.match(/(?<=>)[^<>]+(?=<)/g);
-
-        var originalTag = full.replace(/<a(([\s\S])*?)a>/g, '');
-        var originalRaw = originalTag.match(/(?<=>)[^<>]+(?=<)/g);
-
-        full = translatedTag.replace(translatedRaw, originalRaw);
-        document.getElementsByTagName("h3")[i].innerHTML = full;
+        var subjects = document.getElementsByTagName("h3");
+        modifyListPage(subjects);
     }
-}
+    else
+    {
+    // Modify on Subject Pages
+        if (subjectPageModify == true)
+        {
+            var infobox = document.getElementById("infobox").getElementsByTagName("li");
+            modifySubjectPage(infobox);
+        }
+    }
 
 
+    function modifyListPage(subjects)
+    {
+        for (var i = 0; i <= subjects.length; i++)
+        {
+            var fullTag = subjects[i].innerHTML;
+
+            if (fullTag.match(/<small(([\s\S])*?)small>/g) != null)
+            {
+                var translatedTag = fullTag.replace(/<small(([\s\S])*?)small>/g, '');
+                var translatedRaw = translatedTag.match(/(?<=>)[^<>]+(?=<)/g);
+
+                var originalTag = fullTag.replace(/<a(([\s\S])*?)a>/g, '');
+                var originalRaw = originalTag.match(/(?<=>)[^<>]+(?=<)/g);
+
+                if (listPageModify)
+                {
+                    fullTag = fullTag.replace(originalRaw, 'TBD');
+                    fullTag = fullTag.replace(translatedRaw, originalRaw);
+                    fullTag = fullTag.replace('TBD', translatedRaw);
+                }
+                else
+                {
+                    fullTag = translatedTag.replace(translatedRaw, originalRaw);
+                }
+                document.getElementsByTagName("h3")[i].innerHTML = fullTag;
+            }
+        }
+    }
+
+
+    function modifySubjectPage(infobox)
+    {
+        for (var j = 0; j <= infobox.length; j++)
+        {
+            var currentBox = infobox[j].innerHTML;
+
+            if (currentBox.startsWith("<span class=\"tip\">中文名: </span>"))
+            {
+                document.getElementById("infobox").getElementsByTagName("li")[j].innerHTML = null;
+                break;
+            }
+        }
+    }
+
+    
 })();
