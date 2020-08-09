1612b82c21ecf1fe3de0d59fc9ea7304172c2880
2Jelly2 <2Jelly2@users.noreply.github.com>
2020-08-09 12:36:18 +0800

    Script now supports search pages.

--- Make-Translated-Names-Secondary-or-Removed-on-Bangumi.user.js
+++ Make-Translated-Names-Secondary-or-Removed-on-Bangumi.user.js
@@ -1,95 +1,122 @@
 // ==UserScript==
 // @name         Bangumi 译名次要化或删除
 // @namespace    https://github.com/2Jelly2/Make-Translated-Names-Secondary-or-Removed-on-Bangumi
-// @version      0.03
+// @version      0.04
 // @description  Make Translated Names Secondary or Removed on Bangumi.
 // @author       時計坂しぐれ
+// @grant        none
+
 // @match        https://chii.in/*/list/*
 // @match        https://bgm.tv/*/list/*
 // @match        https://bangumi.tv/*/list/*
+
 // @match        https://chii.in/person/*
 // @match        https://bgm.tv/person/*
 // @match        https://bangumi.tv/person/*
+
 // @match        https://chii.in/subject/*
 // @match        https://bgm.tv/subject/*
 // @match        https://bangumi.tv/subject/*
-// @grant        none
+
+// @match        https://chii.in/subject_search/*
+// @match        https://bgm.tv/subject_search/*
+// @match        https://bangumi.tv/subject_search/*
+
 // ==/UserScript==
 
-(function() {
-    'use strict';
-
-    var url = window.location.href;
-
-    // Exchange original names and translated ones on list pages,
-    // with *false* to remove translated names without exchange.
-    var listPageModify = true;
-
-    // Exchange original names and translated ones on person pages,
-    // with *false* to remove translated names without exchange.
-    var personPageModify = true;
-
-    // Translated names on Subject Pages are remained by default,
-    // with *true* to enable modification.
-    var subjectPageModify = false;
-
-    if (url.match(/(chii.in|bgm.tv|bangumi.tv)\/subject/) == null)
+(
+    function()
     {
-        if (url.match(/(chii.in|bgm.tv|bangumi.tv)\/person/) != null)
-        // Modify on Person Pages
-        {
-            modifyPersonPage();
-        }
-        else
-        // Modify on List Pages
-        {
-            modifyListPage();
-        }
-    }
-    else
-    {
-    // Modify on Subject Pages
-        if (subjectPageModify == true)
-        {
-            modifySubjectPage();
-        }
-    }
+        'use strict';
+
+        var url = window.location.href;
+
+        // Exchange original names and translated ones on specific types of pages,
+        // with *false* to remove translated names without exchange.
+        var listPageModify = true; // List pages
+        var searchPageModify = true; // Search pages
+        var personPageModify = true; // Person pages
+
+        // Translated names on Subject Pages are remained by default,
+        // with *true* to enable modification.
+        var subjectPageModify = false;
 
 
-    function modifyListPage()
-    {
-        var subjects = document.getElementsByTagName("h3");
-
-        for (var i = 0; i < subjects.length; i++)
+        if (url.match(/(chii.in|bgm.tv|bangumi.tv)\/subject\//) == null)
         {
-            var fullTag = subjects[i].innerHTML;
-
-            if (fullTag.match(/<small(([\s\S])*?)small>/g) != null)
+            if (url.match(/(chii.in|bgm.tv|bangumi.tv)\/person\//) != null)
+            // Modify on Person Pages
             {
-                var translatedTag = fullTag.replace(/<small(([\s\S])*?)small>/g, '');
-                var translatedRaw = translatedTag.match(/(?<=>)[^<>]+(?=<)/g);
-
-                var originalTag = fullTag.replace(/<a(([\s\S])*?)a>/g, '');
-                var originalRaw = originalTag.match(/(?<=>)[^<>]+(?=<)/g);
-
-                if (listPageModify)
+                modifyPersonPage();
+            }
+            else
+            {
+                if (url.match(/(chii.in|bgm.tv|bangumi.tv)\/subject_search\//) != null)
+                // Modify on Search Pages
                 {
-                    fullTag = fullTag.replace(originalRaw, 'TBD');
-                    fullTag = fullTag.replace(translatedRaw, originalRaw);
-                    fullTag = fullTag.replace('TBD', translatedRaw);
+                    modifyListPage('Search');
                 }
                 else
+                // Modify on List Pages
                 {
-                    fullTag = translatedTag.replace(translatedRaw, originalRaw);
+                    listPageModify = searchPageModify;
+                    modifyListPage('List');
                 }
-                document.getElementsByTagName("h3")[i].innerHTML = fullTag;
             }
         }
-    }
+        else
+        {
+            if (subjectPageModify == true)
+            // Modify on Subject Pages
+            {
+                modifySubjectPage();
+            }
+        }
 
 
-    function modifyPersonPage()
-    {
+        function modifyListPage(type)
+        {
+            var subjects = document.getElementsByTagName("h3");
+
+            for (var i = 0; i < subjects.length; i++)
+            {
+                var fullTag = subjects[i].innerHTML;
+
+                if (fullTag.match(/<small(([\s\S])*?)small>/g) != null)
+                {
+                    var translatedTag = fullTag.replace(/<small(([\s\S])*?)small>/g, '');
+                    var originalTag = fullTag.replace(/<a(([\s\S])*?)a>/g, '');
+
+                    var originalRaw, translatedRaw;
+                    if (type == 'List')
+                    {
+                        originalRaw = originalTag.match(/(?<=>)[^<>]+(?=<)/g);
+                        translatedRaw = translatedTag.match(/(?<=>)[^<>]+(?=<)/g);
+                    }
+                    else
+                    {
+                        originalRaw = originalTag.match(/(?<=>)[^<>]+(?=<)/g)[1];
+                        translatedRaw = translatedTag.match(/(?<=>)[^<>]+(?=<)/g)[1];
+                    }
+
+                    if (listPageModify)
+                    {
+                        fullTag = fullTag.replace(originalRaw, 'TBD');
+                        fullTag = fullTag.replace(translatedRaw, originalRaw);
+                        fullTag = fullTag.replace('TBD', translatedRaw);
+                    }
+                    else
+                    {
+                        fullTag = translatedTag.replace(translatedRaw, originalRaw);
+                    }
+                    document.getElementsByTagName("h3")[i].innerHTML = fullTag;
+                }
+            }
+        }
+
+
+        function modifyPersonPage()
+        {
             if(url.endsWith("works"))
             {
                 var subjects = document.getElementsByTagName("h3");
@@ -133,23 +160,24 @@
                     }
                 }
             }
-    }
+        }
 
 
-    function modifySubjectPage()
-    {
-        var infobox = document.getElementById("infobox").getElementsByTagName("li");
-
-        for (var j = 0; j < infobox.length; j++)
+        function modifySubjectPage()
         {
-            var currentBox = infobox[j].innerHTML;
+            var infobox = document.getElementById("infobox").getElementsByTagName("li");
 
-            if (currentBox.startsWith("<span class=\"tip\">中文名: </span>"))
+            for (var j = 0; j < infobox.length; j++)
             {
-                document.getElementById("infobox").getElementsByTagName("li")[j].innerHTML = null;
-                break;
+                var currentBox = infobox[j].innerHTML;
+
+                if (currentBox.startsWith("<span class=\"tip\">中文名: </span>"))
+                {
+                    document.getElementById("infobox").getElementsByTagName("li")[j].innerHTML = null;
+                    break;
+                }
             }
         }
-    }
 
-})();
+    }
+)();
